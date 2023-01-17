b70f1bd2b4ae1749fab6dfc8439d26208d543b6a
natew <nateweil1993@gmail.com>
2023-01-16 20:21:51 -0500

    Added error handling, finalized UI interactions

--- GUI main.py
+++ GUI main.py
@@ -1,71 +1,105 @@
 import functions as es
 import PySimpleGUI as ui
+import time as t
 
-label = ui.Text("Type in a task")
+clock = ui.Text("", key="clock")
 
 input_box = ui.InputText(tooltip="This is where you can type in your tasks", key="task")
 
-edit_button = ui.Button("Edit")
-add_button = ui.Button("Add")
-insert_button = ui.Button("Insert")
-delete_button = ui.Button("Remove")
+replace_button = ui.Button("Replace", button_color=("light blue",""))
+add_button = ui.Button("Add", button_color=("light blue",""))
+insert_button = ui.Button("Insert after")
+complete_button = ui.Button("Complete", button_color=("green",""))
 
-task_list = ui.Listbox(values=es.file_io(None, readwrite="r"), key="tasklist", enable_events=True, size=[44, 10])
+task_list = ui.Listbox(values=es.file_io(None, readwrite="r"), key="tasklist", enable_events=True, size=(44, 10))
 
 exit_button = ui.Button("Exit")
+message_label = ui.Text("Nothing to report",key="message")
 
 # I created this variable to make the layout easier to see.
 # The window layout parameter requires a list containing sub-lists. Each sub-list
 # represents a visual row of the interface, lining objects up with each other.
 window_layout = [
-                [label],
+                [clock],
                 [input_box],
-                [edit_button, add_button, insert_button, delete_button],
+                [replace_button, add_button, insert_button, complete_button],
                 [task_list],
-                [exit_button]
+                [exit_button, message_label]
                 ]
 window = ui.Window("It's On My Todo List (c) Nate Weil 2023", layout=window_layout, font=("Helvetica", 16))
 
 
 while True:
 
-    # Loop pauses at the read() line until an event is received
-    event, values = window.read()
-    print(f"Trigger: {event}")
-    print(f"Values: {values}")
+    # Loop pauses at the read() line until an event is received. Timeout allows the loop to proceed (10ms?)
+    event, values = window.read(timeout=10)
+
+    window["clock"].update(value=t.strftime("%b %d, %Y %H:%M:%S"))
+
+    if event != "__TIMEOUT__":
+        print(f"Trigger: {event}")
+        print(f"Values: {values}")
 
     match event:
         case "Add":
-            tasks = es.file_io(None, readwrite="r")
+            if values["task"] != "":
+                tasks = es.file_io(None, readwrite="r")
 
-            new_task = values["task"] + "\n"
-            tasks.append(new_task)
+                new_task = values["task"] + "\n"
+                tasks.append(new_task)
 
-            es.file_io(tasks, readwrite="w")
-            window["tasklist"].update(values=es.file_io(None, readwrite="r"))
+                es.file_io(tasks, readwrite="w")
+                window["tasklist"].update(values=es.file_io(None, readwrite="r"))
+            else:
+                window["message"].update(value="Can't add a blank task!")
 
-        case "Edit":
-            tasks = es.file_io(None, readwrite="r")
+        case "Replace":
+            try:
+                if values["task"] != "":
+                    tasks = es.file_io(None, readwrite="r")
 
-            task_to_edit = values["tasklist"][0]
-            replacement = values["task"] + "\n"
+                    task_to_edit = values["tasklist"][0]
+                    replacement = values["task"] + "\n"
 
-            # You can get the index of an item in a list if you know its value
-            index = tasks.index(task_to_edit)
-            tasks[index] = replacement
+                    # You can get the index of an item in a list if you know its value
+                    index = tasks.index(task_to_edit)
+                    tasks[index] = replacement
 
-            es.file_io(tasks, readwrite="w")
-            window["tasklist"].update(values=es.file_io(None, readwrite="r"))
+                    es.file_io(tasks, readwrite="w")
+                    window["tasklist"].update(values=es.file_io(None, readwrite="r"))
+                else:
+                    window["message"].update(value="Can't replace with a blank task!")
 
-        case "Remove":
-            tasks = es.file_io(None, readwrite="r")
+            except IndexError:
+                ui.Popup("Please select a task first", font=("Helvetica", 16))
 
-            task_to_remove = values["tasklist"][0]
-            index = tasks.index(task_to_remove)
-            tasks.pop(index)
+        case "Complete":
+            try:
+                tasks = es.file_io(None, readwrite="r")
 
-            es.file_io(tasks, readwrite="w")
-            window["tasklist"].update(values=es.file_io(None, readwrite="r"))
+                task_to_remove = values["tasklist"][0]
+                index = tasks.index(task_to_remove)
+                tasks.pop(index)
+
+                es.file_io(tasks, readwrite="w")
+                window["tasklist"].update(values=es.file_io(None, readwrite="r"))
+
+            except IndexError:
+                ui.Popup("Please select a task first", font=("Helvetica", 16))
+
+        case "Insert after":
+            try:
+                tasks = es.file_io(None, readwrite="r")
+                task_to_insert = values["task"] + "\n"
+                previous_task = values["tasklist"][0]
+                index = tasks.index(previous_task) + 1
+                tasks.insert(index, task_to_insert)
+
+                es.file_io(tasks, readwrite="w")
+                window["tasklist"].update(values=es.file_io(None, readwrite="r"))
+
+            except IndexError:
+                ui.Popup("Please select a task first", font=("Helvetica", 16))
 
         case "tasklist":
             selected = values["tasklist"][0].strip("\n")
